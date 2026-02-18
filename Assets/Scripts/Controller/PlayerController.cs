969fb9c1586f37dadd4720b677636823b23cad88
Denoraa <3377545725@qq.com>
2026-02-17 18:41:23 -0800

    1

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    private Rigidbody m_rigidbody;
+    private Camera m_camera;
+
+    public float moveSpeed = 6f;
+    public float acceleration = 20f;
+
+    void Awake()
     {
-        
+        this.m_rigidbody = GetComponent<Rigidbody>();
+       // this.m_camera = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+        
+
+
+
+    }
+
+    private void Inputs()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        // Vector3 input = new Vector3(x,0f,z);
+        // Vector3 dir = Vector3.ClampMagnitude(input,1f);
+
+        // if(dir!=Vector3.Zero)
+        // {
+        //     //this.m_rigidbody.linearVelocity += dir * moveSpeed;
+        // }
+    }
+
+    private bool IsGrounded()
+    {
+        return true;
     }
 }

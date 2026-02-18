7cbaaf4a7f4b75c4c473394f6d66e32d24943a25
lsabe37 <lsabe@uci.edu>
2026-02-17 18:41:41 -0800

    npc dialogue and test scene

---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] private DialogueObject dialogueObject;
+
+    public DialogueUI dialogueUI;
+
+    public void Interact()
+    {
+        //show dialogue
+    }
+
+}
---
+++ Assets/Scripts/Dialogue/DialogueObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueObject : ScriptableObject
+{
+    [SerializeField][TextArea] private string[] dialogue;
+    public string[] Dialogue => dialogue;
+}

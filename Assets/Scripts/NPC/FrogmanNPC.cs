0998b020e9ac127fa5f97d9657c6f3bb02c94045
lsabe37 <lsabe@uci.edu>
2026-03-02 15:16:50 -0800

    fixing files

--- Assets/Scripts/NPC/FrogmanNPC.cs
+++ Assets/Scripts/NPC/FrogmanNPC.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FrogmanNPC : MonoBehaviour
+public class FrogmanNPC : NPC
 {
-    // Start is called before the first frame update
-    void Start()
+    public int desiredItem;
+    public string[] listOfItems;
+    public bool gaveRequest;
+
+    public override void Start()
     {
-        
+        base.Start();
+
+        dialogueManager.interactEvent += SelectDesiredItem;
+        gaveRequest = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SelectDesiredItem()
     {
-        
+        if (gaveRequest == false)
+        {
+            desiredItem = Random.Range(0, listOfItems.Length);
+            gaveRequest = true;
+        }
     }
 }
